17bd107e74d17dbb91c2a5bddc652e61c73a7fe3
cledic <clemente.dicaprio@gmail.com>
2014-08-13 11:26:36 +0200

    Create mk_ramdisk.sh

---
+++ ariettaNews/mk_ramdisk.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo -n "Mounting RAMDisk... "
+
+grep /tmp/tmpdata /etc/mtab > /dev/null
+if [ $? -eq 0 ]; then
+  echo "already mounted..."
+  exit
+fi
+
+if [ ! -e /tmp/tmpdata ]; then
+  mkdir /tmp/tmpdata
+fi
+
+#
+mount -t tmpfs -o size=8M,mode=0744 tmpfs /tmp/tmpdata/
+chmod 777 /tmp/tmpdata/ -R
+
+echo "done!"
+
+
+
