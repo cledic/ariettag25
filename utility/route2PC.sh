03fe759b6412e5be611409bb59f7f52e9d2e661a
cledic <clemente.dicaprio@gmail.com>
2014-08-24 17:57:28 +0200

    Update route2PC.sh

--- utility/route2PC.sh
+++ utility/route2PC.sh
@@ -1,2 +1,6 @@
+# Uso questo script per impostare la rotta di default sull'IP del PC
+# collegato all'Arietta in USB. Per andare in Internet c'è bisogno di abilitare sul PC
+# anche la condivisione Internet sull'interfaccia usata.
+#
 ip route delete
 ip route add default via 192.168.10.20
