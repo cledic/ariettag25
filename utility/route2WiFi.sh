5d2c6e3affcb5b71cc20691fb0ac5f27455f85cb
cledic <clemente.dicaprio@gmail.com>
2014-08-24 17:55:29 +0200

    Update route2WiFi.sh

--- utility/route2WiFi.sh
+++ utility/route2WiFi.sh
@@ -1,2 +1,5 @@
+# Uso questo script per passare la rotta di default sul router WiFi
+# In questo caso il router WiFi ha indirizzo IP: 192.168.1.1
+#
 ip route delete
 ip route add default via 192.168.1.1
