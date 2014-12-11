ff16f7def838a08b296227c163b1fe7b7d3f0998
cledic <clemente.dicaprio@gmail.com>
2014-12-11 14:54:25 +0100

    Create PWM_lib.h

---
+++ pwm_dev/inc/PWM_lib.h
@@ -0,0 +1,7 @@
+/* Enable the PWM channel */
+int PWMChannel_enable( char* chnl);
+/* Set the period value */
+int PWMChannel_SetPeriod( char* chnl, char* period);
+/* Set the duty cycle */
+int PWMChannel_SetDutyCycle( char* chnl, char* duty);
+
