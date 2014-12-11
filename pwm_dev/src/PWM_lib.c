176f22e5fdf22354aa8800cf9f9dd5acd4cdfb3d
cledic <clemente.dicaprio@gmail.com>
2014-12-11 14:53:50 +0100

    Create PWM_lib.c

---
+++ pwm_dev/src/PWM_lib.c
@@ -0,0 +1,137 @@
+#include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+#include <string.h>
+
+#include "PWM_lib.h"
+
+/*
+ echo $PwmChn > /sys/class/pwm/pwmchip0/export
+ 
+ echo 0 > /sys/class/pwm/pwmchip0/pwm$PwmChn/enable
+ echo $Period > /sys/class/pwm/pwmchip0/pwm$PwmChn/period
+ echo $Duty > /sys/class/pwm/pwmchip0/pwm$PwmChn/duty_cycle
+ echo 1 > /sys/class/pwm/pwmchip0/pwm$PwmChn/enable
+
+*/
+
+int PWMChannel_enable( char* chnl)
+{
+    int fchnl;
+	
+    fchnl=open("/sys/class/pwm/pwmchip0/export", O_WRONLY);
+    if (fchnl < 0)
+    {
+    fprintf( stderr, "Cannot open /sys/class/pwm/pwmchip0/export to export it \n");
+    return -1;
+    }
+	
+    // Write the channel you want to export and close the export interface.
+    write(fchnl, chnl, 4);
+    close(fchnl);	
+
+    return 0;
+}
+
+int PWMChannel_SetPeriod( char* chnl, char* period)
+{
+    int fchnl;
+
+    char *buff=(char*)malloc( 255);
+
+    sprintf( buff, "/sys/class/pwm/pwmchip0/pwm%s/enable", chnl);
+
+    fchnl=open(buff, O_WRONLY);
+    if (fchnl < 0)
+    {
+    fprintf( stderr, "Cannot open %s to disable the channel \n", buff);
+    return -1;
+    }
+	
+    // Disable the channel
+    write(fchnl, "0", 2);
+    close(fchnl);	
+	
+    sprintf( buff, "/sys/class/pwm/pwmchip0/pwm%s/period", chnl);
+
+    fchnl=open(buff, O_WRONLY);
+    if (fchnl < 0)
+    {
+    fprintf( stderr, "Cannot open %s to set period \n", buff);
+    return -1;
+    }
+    // 
+    write(fchnl, period, strlen( period));
+    close(fchnl);	
+
+    sprintf( buff, "/sys/class/pwm/pwmchip0/pwm%s/enable", chnl);
+
+    fchnl=open(buff, O_WRONLY);
+    if (fchnl < 0)
+    {
+        fprintf( stderr, "Cannot open %s to re-enable the channel \n", buff);
+        return -1;
+    }
+	
+    // Disable the channel
+    write(fchnl, "1", 2);
+    close(fchnl);	
+
+    return 0;
+}
+
+
+int PWMChannel_SetDutyCycle( char* chnl, char* duty)
+{
+    int fchnl;
+
+    char *buff=(char*)malloc( 255);
+
+    sprintf( buff, "/sys/class/pwm/pwmchip0/pwm%s/enable", chnl);
+
+    fchnl=open(buff, O_WRONLY);
+    if (fchnl < 0)
+    {
+    fprintf( stderr, "Cannot open %s to disable the channel \n", buff);
+    return -1;
+    }
+	
+    // Disable the channel
+    write(fchnl, "0", 2);
+    close(fchnl);	
+	
+    sprintf( buff, "/sys/class/pwm/pwmchip0/pwm%s/duty_cycle", chnl);
+
+    fchnl=open(buff, O_WRONLY);
+    if (fchnl < 0)
+    {
+    fprintf( stderr, "Cannot open %s to set duty_cycle \n", buff);
+    return -1;
+    }
+    // 
+    write(fchnl, duty, strlen( duty));
+    close(fchnl);	
+
+    sprintf( buff, "/sys/class/pwm/pwmchip0/pwm%s/enable", chnl);
+
+    fchnl=open(buff, O_WRONLY);
+    if (fchnl < 0)
+    {
+    fprintf( stderr, "Cannot open %s to re-enable the channel \n", buff);
+    return -1;
+    }
+	
+    // Disable the channel
+    write(fchnl, "1", 2);
+    close(fchnl);	
+
+    return 0;
+}
+
+
