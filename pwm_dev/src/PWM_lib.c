2acc7c472118fcfeae3f731a9841b58d556f9c16
cledic <clemente.dicaprio@gmail.com>
2014-12-27 13:35:24 +0100

    Update PWM_lib.c
    Added omitted free(buff) call. Change from sprintf to snprintf for sanity check. Thanks to AMontefusco. 
    Added null pointer malloc check.

--- pwm_dev/src/PWM_lib.c
+++ pwm_dev/src/PWM_lib.c
@@ -11,9 +11,11 @@
 
 #include "PWM_lib.h"
 
+#define cBUFFER_SIZE    255
+
 /*
  echo $PwmChn > /sys/class/pwm/pwmchip0/export
- 
+
  echo 0 > /sys/class/pwm/pwmchip0/pwm$PwmChn/enable
  echo $Period > /sys/class/pwm/pwmchip0/pwm$PwmChn/period
  echo $Duty > /sys/class/pwm/pwmchip0/pwm$PwmChn/duty_cycle
@@ -24,17 +26,17 @@
 int PWMChannel_enable( char* chnl)
 {
     int fchnl;
-	
+
     fchnl=open("/sys/class/pwm/pwmchip0/export", O_WRONLY);
     if (fchnl < 0)
     {
-    fprintf( stderr, "Cannot open /sys/class/pwm/pwmchip0/export to export it \n");
-    return -1;
+        fprintf( stderr, "Cannot open /sys/class/pwm/pwmchip0/export to export it \n");
+        return -1;
     }
-	
+
     // Write the channel you want to export and close the export interface.
     write(fchnl, chnl, 4);
-    close(fchnl);	
+    close(fchnl);
 
     return 0;
 }
@@ -43,45 +45,56 @@ int PWMChannel_SetPeriod( char* chnl, char* period)
 {
     int fchnl;
 
-    char *buff=(char*)malloc( 255);
+    char *buff=(char*)malloc( cBUFFER_SIZE);
 
-    sprintf( buff, "/sys/class/pwm/pwmchip0/pwm%s/enable", chnl);
+
+    if ( buff==(char*)NULL)
+    {
+        fprintf( stderr, "Cannot allocate memory\n");
+        return -1;
+    }
+
+    snprintf( buff, cBUFFER_SIZE, "/sys/class/pwm/pwmchip0/pwm%s/enable", chnl);
 
     fchnl=open(buff, O_WRONLY);
     if (fchnl < 0)
     {
-    fprintf( stderr, "Cannot open %s to disable the channel \n", buff);
-    return -1;
+        fprintf( stderr, "Cannot open %s to disable the channel \n", buff);
+        free(buff);
+        return -1;
     }
-	
+
     // Disable the channel
     write(fchnl, "0", 2);
-    close(fchnl);	
-	
-    sprintf( buff, "/sys/class/pwm/pwmchip0/pwm%s/period", chnl);
+    close(fchnl);
+
+    snprintf( buff, cBUFFER_SIZE, "/sys/class/pwm/pwmchip0/pwm%s/period", chnl);
 
     fchnl=open(buff, O_WRONLY);
     if (fchnl < 0)
     {
-    fprintf( stderr, "Cannot open %s to set period \n", buff);
-    return -1;
+        fprintf( stderr, "Cannot open %s to set period \n", buff);
+        free(buff);
+        return -1;
     }
-    // 
+    //
     write(fchnl, period, strlen( period));
-    close(fchnl);	
+    close(fchnl);
 
-    sprintf( buff, "/sys/class/pwm/pwmchip0/pwm%s/enable", chnl);
+    snprintf( buff, cBUFFER_SIZE, "/sys/class/pwm/pwmchip0/pwm%s/enable", chnl);
 
     fchnl=open(buff, O_WRONLY);
     if (fchnl < 0)
     {
         fprintf( stderr, "Cannot open %s to re-enable the channel \n", buff);
+        free(buff);
         return -1;
     }
-	
+
     // Disable the channel
     write(fchnl, "1", 2);
-    close(fchnl);	
+    close(fchnl);
+    free(buff);
 
     return 0;
 }
@@ -91,47 +104,55 @@ int PWMChannel_SetDutyCycle( char* chnl, char* duty)
 {
     int fchnl;
 
-    char *buff=(char*)malloc( 255);
+    char *buff=(char*)malloc( cBUFFER_SIZE);
 
-    sprintf( buff, "/sys/class/pwm/pwmchip0/pwm%s/enable", chnl);
+    if ( buff==(char*)NULL)
+    {
+        fprintf( stderr, "Cannot allocate memory\n");
+        return -1;
+    }
+
+    snprintf( buff, cBUFFER_SIZE, "/sys/class/pwm/pwmchip0/pwm%s/enable", chnl);
 
     fchnl=open(buff, O_WRONLY);
     if (fchnl < 0)
     {
-    fprintf( stderr, "Cannot open %s to disable the channel \n", buff);
-    return -1;
+        fprintf( stderr, "Cannot open %s to disable the channel \n", buff);
+        free(buff);
+        return -1;
     }
-	
+
     // Disable the channel
     write(fchnl, "0", 2);
-    close(fchnl);	
-	
-    sprintf( buff, "/sys/class/pwm/pwmchip0/pwm%s/duty_cycle", chnl);
+    close(fchnl);
+
+    snprintf( buff, cBUFFER_SIZE, "/sys/class/pwm/pwmchip0/pwm%s/duty_cycle", chnl);
 
     fchnl=open(buff, O_WRONLY);
     if (fchnl < 0)
     {
-    fprintf( stderr, "Cannot open %s to set duty_cycle \n", buff);
-    return -1;
+        fprintf( stderr, "Cannot open %s to set duty_cycle \n", buff);
+        free(buff);
+        return -1;
     }
-    // 
+    //
     write(fchnl, duty, strlen( duty));
-    close(fchnl);	
+    close(fchnl);
 
-    sprintf( buff, "/sys/class/pwm/pwmchip0/pwm%s/enable", chnl);
+    snprintf( buff, cBUFFER_SIZE, "/sys/class/pwm/pwmchip0/pwm%s/enable", chnl);
 
     fchnl=open(buff, O_WRONLY);
     if (fchnl < 0)
     {
-    fprintf( stderr, "Cannot open %s to re-enable the channel \n", buff);
-    return -1;
+        fprintf( stderr, "Cannot open %s to re-enable the channel \n", buff);
+        free(buff);
+        return -1;
     }
-	
+
     // Disable the channel
     write(fchnl, "1", 2);
-    close(fchnl);	
+    close(fchnl);
+    free(buff);
 
     return 0;
 }
-
-
